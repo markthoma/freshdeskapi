8f75646760ff8d5049842b51ae600cee9bcd7b71
Mark Thomas <32268280+markthoma@users.noreply.github.com>
2018-01-29 15:45:50 +0000

    Add files via upload

---
+++ send_email_attachment.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3.5
+
+import os
+import sys
+import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+COMMASPACE = ', '
+
+def main():
+    sender = 'osiriumbot'
+    password = ''
+    recipients = ['mark.thomas@osirium.com']
+
+    # Create the enclosing (outer) message
+    outer = MIMEMultipart()
+    outer['Subject'] = 'Unapproved Freshdesk Contacts'
+    outer['To'] = COMMASPACE.join(recipients)
+    outer['From'] = sender
+    outer.preamble = 'You will not see this in a MIME-aware mail reader.\n'
+    body = "Hi,\n\nPlease find attached all Freshdesk Contacts that have not verified their email.\n\nMany Thanks"
+    outer.attach(
+    MIMEText(body, 'plain')) # or 'html'
+
+    # List of attachments
+    attachments = ['/home/mark/Documents/Freshdesk/FreshdeskAPI/unverifiededcontacts.txt']
+
+    # Add the attachments to the message
+    for file in attachments:
+        try:
+            with open(file, 'rb') as fp:
+                msg = MIMEBase('application', "octet-stream")
+                msg.set_payload(fp.read())
+            encoders.encode_base64(msg)
+            msg.add_header('Content-Disposition', 'attachment', filename=os.path.basename(file))
+            outer.attach(msg)
+        except:
+            print("Unable to open one of the attachments. Error: ", sys.exc_info()[0])
+            raise
+
+    composed = outer.as_string()
+
+    # Send the email
+    try:
+        with smtplib.SMTP('smtp.gmail.com', 587) as s:
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            s.login(sender, password)
+            s.sendmail(sender, recipients, composed)
+            s.close()
+        print("Email sent!")
+    except:
+        print("Unable to send the email. Error: ", sys.exc_info()[0])
+        raise
+
+if __name__ == '__main__':
+    main()
