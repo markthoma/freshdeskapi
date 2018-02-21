2919069d2a035ab3731ee4d0207f1d5603ff63db
Mark Thomas <mark.thomas@osirium.com>
2018-02-21 15:57:03 +0000

    changed to output csv

--- FreshdeskAPI/freshdesk_api/unapproved_contacts.py
+++ FreshdeskAPI/freshdesk_api/unapproved_contacts.py
@@ -4,6 +4,7 @@ import requests
 import datetime
 import json
 import sys
+import csv
 from urllib.parse import urlencode
 from freshdesk import (
     to_json,
@@ -33,31 +34,27 @@ def get_tags(contact_tag_id):
     response_json = to_json(response)
     return response_json['tags']
 
-def unapproved(contact_list, companies_data, newer_than):
-    #  The file object is never closed and contains
-    #  a hard coded directory.
-    #
-    #  Consider using csv.
-    #
-    #  What happens if the keys don't exist?
-    f = open('/home/mark/Documents/Freshdesk/FreshdeskAPI/unverifiededcontacts.txt','w+')
-    contact_id = 1
-    for contact in contact_list:
-        if contact['active'] == False and datetime.datetime.strptime(
-        contact['created_at'], "%Y-%m-%dT%H:%M:%SZ"
-        ) >= newer_than:
-            contact_tag_id = contact['id']
-            f.write(str(contact_id) + ':')
-            f.write(" Name: " + contact['name'])
-            f.write(" Email: " + contact['email'])
-            company_name = resolve_company_name(companies_data, contact)
-            f.write(" Company: " + str(company_name))
-            f.write(" Created At: " + contact['created_at'])
-            f.write(" Tags: " + str(get_tags(contact_tag_id)) + '\n')
-            contact_id = contact_id + 1
+def create_csv(contact_list, companies_data, newer_than):
+    with open('/home/mark/Documents/Freshdesk/FreshdeskAPI/unverifiededcontacts.csv', 'w') as f:
+        fieldnames = ['Name', 'eMail', 'Company', 'Created at', 'Tags']
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow(fieldnames)
+        for contact in contact_list:
+            if contact['active'] == False and datetime.datetime.strptime(
+            contact['created_at'], "%Y-%m-%dT%H:%M:%SZ"
+            ) >= newer_than:
+                contact_tag_id = contact['id']
+                name = contact['name']
+                email = contact['email']
+                company_name = resolve_company_name(companies_data, contact)
+                company = str(company_name)
+                created_at = contact['created_at']
+                tags = str(get_tags(contact_tag_id))
+                data = [name , email , company , created_at , tags]
+                writer.writerow(data)
 
 if __name__ == '__main__':
     companies_data = get_companies(BASE_URL, AUTH_TUPLE)
     contact_list = all_contacts()
     newer_than = generate_time_delta(30)
-    unapproved(contact_list, companies_data, newer_than)
+    create_csv(contact_list, companies_data, newer_than)
